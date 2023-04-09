9161d374bf23789aa9511f8f3be560e3f10937c0
GaetanDuron <gaetan.duron@gmail.com>
2023-04-09 18:50:47 +0200

    Create function to lauch a quest until puzzle

--- src/quests/quest.ts
+++ src/quests/quest.ts
@@ -1,15 +1,38 @@
-import { Guild, GuildMember, User } from "discord.js";
-import { Quest } from "./types/quest";
+import { GuildMember } from "discord.js";
+import { Quest, ScenarioMessage } from "./types/quest";
+import { setTimeout as sleep } from 'timers/promises'
+import { existsSync } from "fs"; 
+
+const displayScenarioMessage = async (member: GuildMember, scenarioMessage: ScenarioMessage): Promise<null> => {
+  const characterImage = `./src/images/${scenarioMessage.character}_${scenarioMessage.characterEmotion || "default" }.png`
+  const characterDefaultImage = `./src/images/${scenarioMessage.character}_default.png`
+  if (existsSync(characterImage)) {
+    await member.send({files: [characterImage]});
+    await member.send(scenarioMessage.messageBoxContent);
+    await member.send({files: ["./src/images/closing_message_box.png"]});
+  } else if (existsSync(characterDefaultImage)) {
+    await member.send({files: [characterDefaultImage]});
+    await member.send(scenarioMessage.messageBoxContent);
+    await member.send({files: ["./src/images/closing_message_box.png"]});
+  } else {
+    await member.send(`**${scenarioMessage.character}** ${(scenarioMessage.characterEmotion  || "").toLowerCase()}:`);
+    await member.send(scenarioMessage.messageBoxContent);
+  }
+
+  if (scenarioMessage.imageFile) {
+    await member.send({files: [`./src/images/${scenarioMessage.imageFile}`]});
+  }
+  await sleep((scenarioMessage.messageBoxContent.length / 20) * 1000)
+  return null
+}
 
 export const startQuest = async (member: GuildMember, quest: Quest): Promise<null> => {
-  quest.scenario.every((scenarioMessage) => {
-    if (scenarioMessage.expectedResponses != null) {
-      member.send(scenarioMessage.messageBoxContent);
-      return false;
-    }
+  let currentMessageIndex = 0;
+  const responseExpectedMessageIndex = quest.scenario.findIndex(message => message.expectedResponses != null)
 
-    member.send(scenarioMessage.messageBoxContent);
-    return true;
-  })
+  while (currentMessageIndex <= responseExpectedMessageIndex) {
+    await displayScenarioMessage(member, quest.scenario[currentMessageIndex])
+    currentMessageIndex += 1;
+  }
   return null;
 }
